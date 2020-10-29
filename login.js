3a2af22fca7a5896e4d379a7559bd88e08a2aa1f
長澤弘樹 <nagasawa@nagasawahirokinoMacBook-Air.local>
2020-10-29 14:01:29 +0900

    エラーメッセージ追加

--- login.js
+++ login.js
@@ -1,14 +1,17 @@
 window.addEventListener('pageshow', function () {
   // 各要素取得
-  const loginId = document.getElementById("login-id");
+  const loginId = document.getElementById('login-id');
   const password = document.getElementById('login-password');
-  const submitButton = document.getElementById("submit-button");
+  const submitButton = document.getElementById('submit-button');
+
+  const idErrorMessage = document.getElementById('id-error-msg');
+  const pwErrorMessage = document.getElementById('pw-error-msg');
 
   // バリデーション設定
   const idExp = /^([a-zA-Z0-9]{5,})$/;
   const passwordExp = /^([a-zA-Z0-9]{8,})$/;
 
-  // 各ボタンの状態設定
+  // 各状態設定
   submitButton.disabled = true;
 
   // フラグ
@@ -18,7 +21,7 @@ window.addEventListener('pageshow', function () {
   // 各要素のイベント追加
   loginId.addEventListener('change', checkIdExp);
   password.addEventListener('change', checkPassExp);
-  submitButton.addEventListener("click", submit);
+  submitButton.addEventListener('click', submit);
 
   /**
    * IDのバリデーションチェック
@@ -27,11 +30,13 @@ window.addEventListener('pageshow', function () {
     if (idExp.test(loginId.value)) {
       console.log('id:success');
       isIdSuccess = true;
+      if (!idErrorMessage.classList.contains('hidden-message')) { idErrorMessage.classList.add('hidden-message'); }
       checkSuccess();
     }
     else{
       console.log('id:invalid');
       isIdSuccess = false;
+      if (idErrorMessage.classList.contains('hidden-message')) { idErrorMessage.classList.remove('hidden-message'); }
       checkSuccess();
     }
   }
@@ -42,11 +47,13 @@ window.addEventListener('pageshow', function () {
     if (passwordExp.test(password.value)) {
       console.log('pw:success');
       isPwSuccess = true;
+      if (!pwErrorMessage.classList.contains('hidden-message')) { pwErrorMessage.classList.add('hidden-message'); }
       checkSuccess();
     }
     else{
       console.log('pw:invalid');
       isPwSuccess = false;
+      if (pwErrorMessage.classList.contains('hidden-message')) { pwErrorMessage.classList.remove('hidden-message'); }
       checkSuccess();
     }
   }
@@ -69,7 +76,7 @@ window.addEventListener('pageshow', function () {
    * ログインボタンを押した時の挙動
    */
   function submit() {
-    console.log("ID:" + loginId.value);
-    console.log("password:" + password.value);
+    console.log('ID:' + loginId.value);
+    console.log('password:' + password.value);
   }
 });
